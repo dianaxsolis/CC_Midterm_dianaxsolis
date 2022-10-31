17b58e523ef345f1d9095bad5df9a4886d38a5c8
diana solis <68870429+dianaxsolis@users.noreply.github.com>
2022-10-31 02:14:37 -0400

    completed_first_scene_draft

--- sweatDrops.js
+++ sweatDrops.js
@@ -1,35 +1,123 @@
 class sweatDrops {
 
-	constructor(){
+  constructor(x,y,s){
 
-		this.acceleration = createVector(0,0.5)
-	}
+    // variables 
 
-	
-	display(){
+    this.x_ = x;
+    this.y_= y;
 
-	// creating the base face structure
+    // movement variables
 
-	// face structure
+    this.speed = s;
+    this.position = createVector(random(0,width), this.y_);
+    this.velocity = createVector(this.speed, this.speed);
+  }
 
-	fill (248, 179, 143);
-	arc (700,0,700,1000,0,PI+QUARTER_PI,CHORD);
+  update(){
+    
+    this.position.add(this.velocity);
 
-	// right eye structure
+  }
 
-	fill (159,64,83); - // color eye
-	stroke (137,48,49);
-	// fill(0); // black eye color fill
-	strokeWeight(4);
-	ellipse (820,0,50,200);
+  // drawing out the structure of the first scene
 
-	// left eye structure
+  display (){
 
-	fill (159,64,83);
-	stroke(137,48,49);
-	strokeWeight(4);
-	ellipse (600,0,50,200);
+  // creating the base face structure
 
-	}
+  // face structure
+
+  fill (248, 179, 143);
+  arc (700,0,700,1000,0,PI+QUARTER_PI,CHORD);
+
+  // right eye structure
+
+  // fill (159,64,83); - // color eye
+  // stroke (137,48,49);
+    stroke(255);
+  fill(0); // black eye color fill
+  strokeWeight(4);
+  ellipse (820,0,50,200);
+
+  // left eye structure
+
+  // fill (159,64,83);
+  fill(0);
+  // stroke(137,48,49);
+    stroke(255);
+  strokeWeight(4);
+  ellipse (600,0,50,200);
+    
+    fill (39,170, 226);
+    noStroke();
+    ellipse (this.x_,this.y_,50,50)
+    triangle(this.x_-25,this.y_, this.x_-25,this.y_-50,this.x_+20,this.y_-15);
+    fill (255);
+    arc (this.x_,this.y_,15,25,0,PI+QUARTER_PI,PIE);
+    
+    stroke(0);
+    strokeWeight(2);
+    vertex(600,200);
+    bezierVertex(700,180,750,200,780,180);
+      
+    stroke(0);
+    noFill();
+    strokeWeight(2);
+    beginShape();
+    // line (600,300,800,300);
+    vertex(600,250);
+    curveVertex(600,300);
+    curveVertex(625,290);
+    curveVertex(650,280);
+    curveVertex(675,260);
+    curveVertex(700,250);
+    curveVertex(725,260);
+    curveVertex(750,280);
+    curveVertex(800,290);
+    curveVertex(825,300);
+    
+    endShape();
+      
+}
+
+  // end of drawing structure
+
+  checkEdges(){ // repositions fire
+    if(this.position.x_ > width){
+      this.position.x_ = 0;
+
+    }else if(this.position.x_ < 0){
+      this.position.x_ = width;
+
+    }
+    if(this.position.y_ > height){
+      this.position.y_ = 0;
+
+    }else if(this.position.y_ < 0){
+      this.position.y_ = height;
+    }
+
+  }
+
+  move(){
+      this.y_=this.y_+this.speed;
+      
+        
+      if (this.y_>600){
+        this.y_ = 0;
+    }
+}
+
+/*
+function droplet (x, y){
+
+  noStroke();
+  fill (39,170, 226);
+  // vertex(500,50);
+  // bezierVertex(this.position.x_ + 300, this.position.y -150, this.position.x +250, this.position.y -100, this.position.x +300, this.position.y -100, this.position.x +400, this.position.y -100,600);
+  ellipse (200,200,50,100);
+    
+    */
 
 }
--- tickTock.js
+++ tickTock.js
@@ -4,23 +4,71 @@ Diana Solis - Section A - Creative Coding - Midterm Project: The Unexpected Mach
 
 Resources used to complete project: 
 1. The Coding Train: 6.2: Classes in JavaScript with ES6 - p5.js tutorial
+2. The Coding Train: 6.3: Constructor Agreements with Classes in JavaScript - p5.js tutorial
 
 
 */
 
-let sweat
+let sweat1;
+let sweat2;
+let sweat3;
+
+let sweat4;
+let sweat5;
+let sweat6;
+
+let sweat7;
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
-  background(255);
+  createCanvas(1500,800);
+  // background(255);
 
-  sweat = new sweatDrops();
+  sweat1 = new sweatDrops(480,50,5);
+  sweat2 = new sweatDrops(500,50,8);
+  sweat3 = new sweatDrops(460,20,12);
+  
+  sweat4 = new sweatDrops (900,50,12);
+  sweat5 = new sweatDrops (950,20,8);
+  sweat6 = new sweatDrops (955,50,5);
+  
+  sweat7 = new sweatDrops(650,20,15);
+  
   
 }
 
 function draw() {
+          
+  background(255);
   
-  sweat.display();
+  sweat1.display();
+  sweat2.display();
+  sweat3.display();
+  
+  sweat4.display();
+  sweat5.display();
+  sweat6.display();
+  
+  sweat7.display();
+  
+  sweat1.move();
+  sweat2.move();
+  sweat3.move();
+  
+  sweat4.move();
+  sweat5.move();
+  sweat6.move();
+  
+  sweat7.move();
+  
+  
+  
+  /*
 
+  for (let j = 0; j < 1200; j++){
+    sweat[j].update();
+    sweat[j].display();
+    sweat[j].checkEdges();
+  }
+  */
 
 }
